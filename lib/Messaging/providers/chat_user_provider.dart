f269e04f200b34b390173c3800d301dcf6b481b6
Lenz Paul <lenz@pop-os.localdomain>
2021-03-31 10:14:13 -0700

    providers starter code

--- lib/Messaging/providers/chat_user_provider.dart
+++ lib/Messaging/providers/chat_user_provider.dart
@@ -4,25 +4,16 @@ import 'package:flutter_chat_app/core/models/chat_user.dart';
 import 'package:flutter_chat_app/core/services/database.dart';
 import 'package:provider/provider.dart';
 
+// TODO: Create StateLess widget `ChatUserProvider` with required member `child` of type Widget
+// TODO: Use provider to get current value of Firbase User
+// TODO: Create Future Provider to fetch value of `ChatUser` using `Database.getUserFromDb`
+//        passing it the `uid` of currently logged firebase user
+// TODO: Pass child as child
+
 class ChatUserProvider extends StatelessWidget {
   /// Fetches the remote data about this [ChatUser].
   ///
   /// Once the data is fetched (i.e. the future is completed),
   /// it injects the data into the subtree as a [ChatUser].
-  ChatUserProvider({@required this.child});
-  final Widget child;
 
-  @override
-  Widget build(BuildContext context) {
-    /// The currently logged in [User].
-    User firebaseUser = Provider.of<User>(context);
-
-    return FutureProvider<ChatUser>.value(
-      value: Database().getUserFromDb(uid: firebaseUser.uid),
-      catchError: (context, error) {
-        throw 'FutureProvider Error: ${error.toString()}';
-      },
-      child: child,
-    );
-  }
 }
--- lib/Messaging/providers/messages_provider.dart
+++ lib/Messaging/providers/messages_provider.dart
@@ -7,44 +7,16 @@ import 'package:flutter_chat_app/core/services/database.dart';
 import 'package:flutter_chat_app/core/views/loading_screen.dart';
 import 'package:provider/provider.dart';
 
-class MessageProviders extends StatelessWidget {
-  /// Injects providers of [MessagesReceived], [MessagesSent] and [List\<ChatUser\>]
-  MessageProviders({@required this.child});
-  final Widget child;
+// TODO: Create statless widgeet `MessageProviders` with `child` member of type `widget`
+//
+// TODO: Declare instance of `Database` db
+// TODO: Using Provider, get the value of `ChatUser` and store it in a var `chatUser`
+// TODO: If/while chatUser is null (is async so during fetch), return `LoadingScreen` widget
+// TODO: Otherwise, return a MultiProvider
+//          - StreamProvider<MessagesSent>
+//          - StreamProvider<MessagesReceived>
+//          - StreamProvider<List<ChatUser>>
+// TODO: Set `lazy` parameters to false
+// TODO: Set child to child
 
-  @override
-  Widget build(BuildContext context) {
-    /// The currently logged in [ChatUser].
-    ChatUser chatUser = Provider.of<ChatUser>(context); // i.e. listen:true
-
-    /// Wait for the value of chatUser to be available.
-    if (chatUser == null) return LoadingScreen();
-
-    Database db = Database();
-    return MultiProvider(
-      providers: [
-        /// Track the value of the list of messages sent to this user.
-        StreamProvider<MessagesReceived>.value(
-          value: db.getMessagesSentToUser(chatUser: chatUser),
-          lazy: false,
-        ),
-
-        /// Track the value of the list of messages sent by this user.
-        StreamProvider<MessagesSent>.value(
-          value: db.getMessagesSentByUser(chatUser: chatUser),
-          lazy: false,
-        ),
-
-        /// Track the value of the list of all users.
-        // StreamProvider<List<ChatUser>>.value(value: db.getUserList()),
-        StreamProvider<List<ChatUser>>.value(
-          value: db.getUserList(),
-
-          /// Get the value right away, as opposed to waiting for a `Provider.of` call
-          lazy: false,
-        ),
-      ],
-      child: child,
-    );
-  }
-}
+class MessageProviders extends StatelessWidget {}
